675b792f9db6fbdde6bf7555e3cc8b0499410c37
DrKazza <github@DrKazza.com>
2024-03-13 16:30:36 +0000

    get markets working

---
+++ instrumentIDLists.py
@@ -0,0 +1,115 @@
+aevoInstIDList = {
+    '10000SATS-PERP': '13882' ,
+    '1000BONK-PERP': '11312' ,
+    '1000PEPE-PERP': '7020' ,
+    'AEVO-PERP': '22007' ,
+    'ALT-PERP': '17513' ,
+    'APT-PERP': '11642' ,
+    'AR-PERP': '20735' ,
+    'ARB-PERP': '7349' ,
+    'ATOM-PERP': '13987' ,
+    'AVAX-PERP': '9060' ,  
+    'AXL-PERP': '21084' ,
+    'BANANA-PERP': '7623' ,
+    'BEAM-PERP': '12478' ,
+    'BEAMX-PERP': '12522' ,
+    'BITCOIN-PERP': '8833' ,
+    'BLAST-PERP': '12418' ,
+    'BLUR-PERP': '10008' ,
+    'BLZ-PERP': '8583' ,
+    'BNB-PERP': '4042' ,
+    'BTC-PERP': '3396' ,
+    'CANTO-PERP': '8797' ,
+    'CRV-PERP': '5757' ,
+    'DOGE-PERP': '11969' ,
+    'DYDX-PERP': '9358' ,
+    'DYM-PERP': '15822' ,
+    'ETH-PERP': '1' ,
+    'FRIEND-PERP': '6815' ,
+    'GLMR-PERP': '8552' ,
+    'HIFI-PERP': '8139' ,
+    'HPOS-PERP': '8802' ,
+    'ILV-PERP': '12479' ,
+    'INJ-PERP': '10009' ,
+    'JITO-PERP': '13193' ,
+    'JUP-PERP': '12938' ,
+    'LDO-PERP': '11460' ,
+    'LINK-PERP': '8494' ,
+    'MANTA-PERP': '16207' ,
+    'MATIC-PERP': '5125' ,
+    'MEME-PERP': '10504' ,
+    'MINA-PERP': '10280' ,
+    'MKR-PERP': '8341' ,
+    'MYRO-PERP': '21526' ,
+    'NMR-PERP': '7480' ,
+    'NTRN-PERP': '11849' ,
+    'OP-PERP': '7348' ,
+    'ORDI-PERP': '13194' ,
+    'OX-PERP': '8543' ,
+    'PARCL-PERP': '20289' ,
+    'PENDLE-PERP': '21316' ,
+    'PIXEL-PERP': '18892' ,
+    'PORTAL-PERP': '20181' ,
+    'PRIME-PERP': '9764' ,
+    'PYTH-PERP': '11768' ,
+    'RNDR-PERP': '13091' ,
+    'SEI-PERP': '6104' ,
+    'SHFL-PERP': '19417' ,
+    'SOL-PERP': '5197' ,
+    'STRAX-PERP': '9605' ,
+    'STRK-PERP': '19174' ,
+    'SUI-PERP': '17791' ,
+    'SYN-PERP': '7530' ,
+    'T-PERP': '9451' ,
+    'TAO-PERP': '11070' ,
+    'TIA-PERP': '8588' ,
+    'TON-PERP': '20813' ,
+    'TRB-PERP': '9924' ,
+    'TRX-PERP': '12123' ,
+    'UMA-PERP': '17792' ,
+    'W-PERP': '22006' ,
+    'WIF-PERP': '21317' ,
+    'WLD-PERP': '5514' ,
+    'XRP-PERP': '5196' ,
+    'ZETA-PERP': '18342' }
+
+# fix this:
+# dydxInsts = [
+# SUSHI-USD
+# LTC-USD
+# ALGO-USD
+# MKR-USD
+# CELO-USD
+# ICP-USD
+# BCH-USD
+# LINK-USD
+# DOGE-USD
+# MATIC-USD
+# COMP-USD
+# XTZ-USD
+# YFI-USD
+# ADA-USD
+# SOL-USD
+# AAVE-USD
+# XLM-USD
+# AVAX-USD
+# XMR-USD
+# ATOM-USD
+# EOS-USD
+# LUNA-USD
+# ENJ-USD
+# SNX-USD
+# NEAR-USD
+# 1INCH-USD
+# ZRX-USD
+# UNI-USD
+# TRX-USD
+# DOT-USD
+# ETH-USD
+# CRV-USD
+# ZEC-USD
+# UMA-USD
+# RUNE-USD
+# FIL-USD
+# BTC-USD
+# ETC-USD]
